dbf51d9608ad1daeb171750d22c643cd4f83536a
John Mistica <47683574+johndev4@users.noreply.github.com>
2020-08-03 22:30:10 +0800

    update

--- js/nav-script.js
+++ js/nav-script.js
@@ -26,22 +26,57 @@ $(document).ready(function () {
             $(this).animate({
                 border: "1px solid white"
             }, 500);
-        } else{
+        } else {
             $(this).animate({
                 opacity: "1.0"
             }, 250);
         }
     });
 
-    window.onscroll = function() {
-        if ($(this).scrollTop() > 0){
+    /*$(document).on("scroll", function() {
+        if ($(this).scrollTop() > 1){
             $('.brand-name').animate({
                 opacity: "0",
                 width: "0px"
             }, 500);
-        } else if ($(this).scrollTop() == 0){
-            $('.brand-name').animate({width: "200px"}, 500);
+
+        } else{
+            $('.brand-name').animate({
+                opacity: "1.0",
+                width: "200px"
+            }, 500);
         }
-    }
+    });*/
+
+    $('.navbar-toggler').first().click(function () {
+        if ($(this).hasClass("collapsed")){
+            $('#rotate-box').css({
+                //for firefox
+                "-moz-animation-name": "rotate",
+                "-moz-animation-duration": "0.5s",
+                "-moz-animation-iteration-count": "1",
+                "-moz-animation-fill-mode": "forwards",
+                //for safari & chrome
+                "-webkit-animation-name": "rotate",
+                "-webkit-animation-duration": "0.5s",
+                "-webkit-animation-iteration-count": "1",
+                "-webkit-animation-fill-mode": "forwards",
+            });
+
+        } else{
+            $('#rotate-box').css({
+                //for firefox
+                "-moz-animation-name": "unrotate",
+                "-moz-animation-duration": "0.5s",
+                "-moz-animation-iteration-count": "1",
+                "-moz-animation-fill-mode": "forwards",
+                //for safari & chrome
+                "-webkit-animation-name": "unrotate",
+                "-webkit-animation-duration": "0.5s",
+                "-webkit-animation-iteration-count": "1",
+                "-webkit-animation-fill-mode": "forwards",
+            });
+        }
+    });
 
 });
