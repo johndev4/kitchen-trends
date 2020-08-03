66a6621fd2d66a3b4db813ebdc8cdfbc0df01cd7
John Mistica <47683574+johndev4@users.noreply.github.com>
2020-08-03 15:44:57 +0800

    Update

--- js/nav-script.js
+++ js/nav-script.js
@@ -1,14 +1,25 @@
-$(document).ready(function() {
-    $('.nav-link').hover(function() {
+$(document).ready(function () {
+
+    $('.nav-link').hover(function () {
         $(this).find(".nav-link-bottom").css("visibility", "visible");
         $(this).find(".nav-link-bottom").animate({
             width: "100%"
         }, 250);
 
-    }, function() {;
+    }, function () {
+        ;
         $(this).find(".nav-link-bottom").animate({
             width: "0px"
         }, 250);
         $(this).find(".nav-link-bottom").css("visibility", "hidden")
     });
+
+    //products dropdown
+    $('.dropdown').hover(function () {
+        $(this).find(".dropdown-menu").show();
+
+    }, function () {
+        $(this).find(".dropdown-menu").hide();
+    });
+
 });
