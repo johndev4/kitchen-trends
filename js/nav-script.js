c6e1f3603c169583f5fc783bdb43a2f0fabcecc2
John Mistica <47683574+johndev4@users.noreply.github.com>
2020-08-03 18:59:06 +0800

    update

--- js/nav-script.js
+++ js/nav-script.js
@@ -21,4 +21,27 @@ $(document).ready(function () {
         $(this).find(".dropdown-menu").hide();
     });
 
+    $('.navbar-toggler').on("click", function () {
+        if (!$('.navbar-collapse').hasClass("show")) {
+            $(this).animate({
+                border: "1px solid white"
+            }, 500);
+        } else{
+            $(this).animate({
+                opacity: "1.0"
+            }, 250);
+        }
+    });
+
+    window.onscroll = function() {
+        if ($(this).scrollTop() > 0){
+            $('.brand-name').animate({
+                opacity: "0",
+                width: "0px"
+            }, 500);
+        } else if ($(this).scrollTop() == 0){
+            $('.brand-name').animate({width: "200px"}, 500);
+        }
+    }
+
 });
