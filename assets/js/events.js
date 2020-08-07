17e6863f72f6697495c7d18ec9e9429d71322595
John Mistica <47683574+johndev4@users.noreply.github.com>
2020-08-07 16:20:05 +0800

    update files

---
+++ assets/js/events.js
@@ -0,0 +1,93 @@
+$(document).ready(function () {
+
+    $(window).on("load", function() {
+        $('.feature-text').animate({
+            opacity: "1.0",
+            top: "20%"
+        }, 1500);
+
+        showToTopBtn();
+    });
+
+    $(window).on("scroll", showToTopBtn);
+
+    /*hover event for products category*/
+    $('.products-category').hover(function () {
+        $(this).find('.border-bottom-hr').css({ visibility: "visible" });
+        $(this).find('.border-bottom-hr').animate({
+            width: "100%"
+        }, 250);
+    }, function () {
+        $(this).find('.border-bottom-hr').animate({
+            width: "0px"
+        }, 250);
+        $(this).find('.border-bottom-hr').css({ visibility: "hidden" });
+    });
+
+    /*click event for navigation to top*/
+    $('#to-top').click(function () {
+        $('html, body').animate({
+            scrollTop: 0
+        }, "slow");
+    });
+
+    /*hover event for footer links*/
+    $('.footer-link').hover(function () {
+        $(this).css({
+            color: "#cccccc",
+            "text-decoration": "underline"
+        });
+    }, function () {
+        $(this).css({
+            color: "#eeeeee",
+            "text-decoration": "none"
+        });
+    });
+
+    /*event click for navigation toggler*/
+    $('.navbar-toggler').first().click(function () {
+        if ($(this).hasClass("collapsed")) {
+            $('#rotate-box').css({
+                "animation-name": "rotateClockwise",
+                "animation-duration": "0.1s",
+                "animation-iteration-count": "1",
+                "animation-fill-mode": "forwards"
+            });
+
+            $('#rotate-box').removeClass("fa-bar");
+            $('#rotate-box').addClass("fa-times");
+
+        } else {
+            $('#rotate-box').css({
+                "animation-name": "rotateCounterClockwise",
+                "animation-duration": "0.1s",
+                "animation-iteration-count": "1",
+                "animation-fill-mode": "forwards"
+            });
+
+            $('#rotate-box').removeClass("fa-times");
+            $('#rotate-box').addClass("fa-bar");
+        }
+    });
+
+    /*hover event for navigation link*/
+    $('.nav-link').hover(function () {
+        $(this).find('.border-bottom-hr').css({visibility: "visible"});
+        $(this).find('.border-bottom-hr').animate({
+            width: "100%"
+        }, 250);
+    }, function() {
+        $(this).find('.border-bottom-hr').animate({
+            width: "0px"
+        }, 250);
+        $(this).find('.border-bottom-hr').css({visibility: "hidden"});
+    });
+
+    /*click event to navigate to products*/
+    $('.nav-to-products').click(function() {
+        $('html, body').animate({
+            scrollTop: $('#prod-cat-sec').position().top
+        }, "slow");
+    });
+
+});
--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,22 +1,11 @@
 $(document).ready(function () {
 
-    $(window).on("scroll", showToTopBtn);
-    showToTopBtn();
-
-    //Go to top script--
-    $('#to-top').click(function () {
-        $('html, body').animate({
-            scrollTop: 0
-        }, "slow");
-    });
-
     function showToTopBtn() {
         if ($(window).scrollTop() > 80) {
             $('.to-top.ovr-container').fadeIn("slow", "linear");
         } else {
             $('.to-top.ovr-container').fadeOut("slow", "linear");
         }
-        //$('#to-top').text($(window).scrollTop());
     }
-
+    
 });
